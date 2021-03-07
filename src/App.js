9202aab601a15243506d714f5c173d09aca38d0c
Rendy Artha <rendyartha312@gmail.com>
2021-03-07 22:58:42 +0700

    menampilkan data ke xml

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import { connect } from 'react-redux';
+import { Card, Container } from 'react-bootstrap';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      test
+      <h1>Kumpulan Buku:</h1>
+      {props.state.map((judulBuku) => (
+        <Container key={judulBuku.id}>
+          <Card style={{ width: '18rem', marginBottom: '1rem' }}>
+            <Card.Body>
+              <Card.Title>{judulBuku.judul}</Card.Title>
+              <Card.Text>
+                {judulBuku.penerbit}
+              </Card.Text>
+              <Card.Text>
+                {judulBuku.harga}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      ))}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    state: state.Title
+  }
+}
+
+// const mapDispatchToProps = () => {
+
+// }
+
+export default connect(
+  mapStateToProps, 
+  null
+)
+(App);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import Store from './redux/Store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={Store}>
